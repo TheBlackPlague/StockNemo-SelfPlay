e85b03b58522a032d711cb9abb50f777165b44d7
Shaheryar Sohail <shaheryarsohailtbp@gmail.com>
2022-07-02 02:46:56 -0400

    Adding games & markdown displayer.

---
+++ MarkdownGeneration.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Backend;
+
+public static class MarkdownGeneration
+{
+
+    private const int START = 0;
+    private const int LIMIT = 800;
+    private const string PATH = ".\\Markdown.md";
+
+    public static void Main()
+    {
+        Console.WriteLine("Generating string...");
+        StringBuilder sb = new();
+        int lastSection = 0;
+        for (int i = START; i < LIMIT; i++) {
+            if (i % 4 == 0) {
+                sb.Append("| ");
+                lastSection = i;
+            }
+
+            
+            sb.Append("<img src=\"GIF/" + i + ".gif\" alt=\"GAME-" + i + "\" width=\"180\" height=\"210\"/>");
+            sb.Append(" |");
+            if (i == lastSection + 3) {
+                sb.Append('\n');
+            }
+            if (i == 3) {
+                sb.Append("|--------------------------------------------------------------|")
+                    .Append("--------------------------------------------------------------|")
+                    .Append("--------------------------------------------------------------|")
+                    .Append("--------------------------------------------------------------|\n");
+            }
+        }
+        Console.WriteLine("String generated.");
+
+        File.WriteAllText(PATH, sb.ToString());
+    }
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # StockNemo-SelfPlay
 Contains StockNemo Games from Self-Play (each version >= 2.0.0.3) generated using StockNemo's PgnToGif utility.
+
+- [StockNemo 2.0.0.3](2.0.0.3/GAMES.md)
